007428d4d9f32cdd282d099ac75d5763924bacef
Igor <IGOS.321@gmail.com>
2018-08-17 16:50:20 +0300

    Fix bootstrap(2) Example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import '@babel/polyfill'
 import Vue from 'vue';
-import './plugins/bootstrap-vue'
 import 'normalize.css/normalize.css';
+import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <el-button>Hello!</el-button>
+    <b-container>
+      <b-row>
+        <b-col>1 of 2</b-col>
+        <b-col>2 of 2</b-col>
+      </b-row>
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
