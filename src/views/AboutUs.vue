3ae8d6ea52afdeacb4d3b8421b86e4354fad0182
virvira <varviko.mx@gmail.com>
2018-08-30 01:14:52 +0300

    moved header and footer, add AboutUs and Contacts

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="about">
+    <b-container>
+      <section class="aboutus">
+        <div class="aboutus__wrapper">
+          <h2 class="aboutus__title">О нас</h2>
+          <p class="aboutus__description">
+            Компания Jumble Sweets основана в городе Харькове в 2018-ом году.<br>
+            <br>
+            Наша миссия - поставлять самые разнообразные сладости со всего мира в Украину. В нашем каталоге представлено более 10 видов товара. Именно наша компания поможет Вам с лёгкостью справиться с предпраздничной суетой.<br>
+            <br>
+            Вдохните глоток свежего креатива и отойдите в сторону от банальных сюрпризов. Jumble Sweets дарит радость от подарков всем желающим.
+          </p>
+          <div class="row aboutus__schedule">
+            <div class="aboutus__item">
+              <div class="aboutus__item-icon">
+                <img src="../assets/Icons/Calendar.svg" width="60" height="60" alt="Дни работы">
+              </div>
+              <div class="aboutus__item-wrapper">
+                <h3 class="aboutus__item-title">Дни работы</h3>
+                <p class="aboutus__item-subtitle">С понедельника по воскресенье</p>
+              </div>
+            </div>
+            <div class="aboutus__item">
+              <div class="aboutus__item-icon">
+                <img src="../assets/Icons/Clock.svg"  width="60" height="60" alt="Время работы">
+              </div>
+              <div class="aboutus__item-wrapper">
+                <h3 class="aboutus__item-title">Время работы</h3>
+                <p class="aboutus__item-subtitle">Работаем с 10:00 до 22:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="support">
+        <div class="support__wrapper">
+          <h2 class="support__title">Поддержка и отзывчивость</h2>
+          <p class="support__description">
+            Клиенты Jumble Sweets всегда могут получить консультацию от наших менеджеров, достаточно позвонить по одному из указанных на сайте номеров или написать нам на почту pochta@gmail.com. Менеджер поможет Вам подобрать нужный товар и ответит на всё интересующие Вас вопросы.<br>
+            <br>
+            Jumble Sweets дорожит доверием клиентов, мы полностью отвечаем за подлинность и качество предлагаемого товара. Также мы следим за новыми течениями и постоянно расширяем ассортимент нашего магазина, учитывая пожелания клиентов.
+          </p>
+        </div>
+      </section>
+
+    </b-container>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'AboutUs',
+    components: {
+    },
+  };
+</script>
+
+<style lang="sass">
+
+
+</style>
---
+++ src/views/Contacts.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="contacts">
+    <b-container>
+      <!--<div class="map">-->
+        <!--<iframe src="https://goo.gl/maps/eHffYJ9gW2M2" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>-->
+      <!--</div>-->
+      <yandex-map
+        :coords="[50.029227, 36.327978]"
+        zoom="17"
+        style="width: 100%; height: 500px;"
+        :cluster-options="{
+                1: {clusterDisableClickZoom: true}
+              }"
+        :behaviors="['drag']"
+        :controls="[]"
+        :placemarks="placemarks"
+        map-type="map"
+      >
+
+      </yandex-map>
+
+    </b-container>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Contacts',
+    data() {
+      return {
+        placemarks: [
+          {
+            coords: [50.029227, 36.327978],
+            properties: {}, // define properties here
+            options: {}, // define options here
+            clusterName: '1',
+            callbacks: {
+              click() {
+              },
+            },
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style lang="sass">
+
+
+</style>
