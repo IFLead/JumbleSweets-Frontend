2bb76e1d95288312ee2bb0bb88d66b8829620203
virvira <varviko.mx@gmail.com>
2018-09-03 00:42:36 +0300

    add delivery

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import AboutUs from './views/AboutUs.vue';
 import Contacts from './views/Contacts.vue';
+import DelPay from './views/DelPay.vue';
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/contacts',
       name: 'contacts',
       component: Contacts,
+    },
+    {
+      path: '/delivery',
+      name: 'delivery',
+      component: DelPay,
     },
     // {
     //   path: '/about',
