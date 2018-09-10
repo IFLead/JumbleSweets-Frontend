849fae135a9f6b22de28f2fa1be0f0fb65608525
Gumbeat <leo2598@yandex.ua>
2018-09-10 20:43:22 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
+import YmapPlugin from 'vue-yandex-maps';
 import 'normalize.css/normalize.css';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 import './registerServiceWorker';
 import './plugins/element';
 import './styles/element-variables.scss';
-import YmapPlugin from 'vue-yandex-maps';
+import { createProvider } from './vue-apollo';
 
 Vue.config.productionTip = false;
 
@@ -17,8 +18,7 @@ Vue.use(YmapPlugin);
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App),
 }).$mount('#app');
-
-
 
