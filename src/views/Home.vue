e10f4687c234dc29ca15ba97a69d594707dbb35e
Gumbeat <leo2598@yandex.ua>
2018-09-14 21:33:02 +0300

    лайки на карточках

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -341,17 +341,27 @@
                           </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
                         </svg>
                       </div>
-                      <!--todo добавить состояние кнопки для продукта в избранном-->
-                      <div v-if="product.is_top">
+                      <div >
                       </div>
                       <div class="catalog__like">
-                        <svg id="Capa_1" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                             width="21.74px" height="20px" viewBox="0 0 510 510" style="enable-background:new 0 0 510 510;" xml:space="preserve">
-                          <g>
-                            <g id="favorite">
-                              <path class="like" d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
+                        <div v-if="product.is_top">
+                          <svg id="Capa_1" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                               width="21.74px" height="20px" viewBox="0 0 510 510" style="enable-background:new 0 0 510 510;" xml:space="preserve">
+                            <g>
+                              <g id="favorite">
+                                <path class="liked" d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
 																	C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
-                        </svg>
+                          </svg>
+                        </div>
+                        <div v-else>
+                          <svg id="Capa_1" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                               width="21.74px" height="20px" viewBox="0 0 510 510" style="enable-background:new 0 0 510 510;" xml:space="preserve">
+                            <g>
+                              <g id="favorite">
+                                <path class="like" d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
+																	C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                          </svg>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -517,7 +527,7 @@ export default {
   methods: {
     ...mapActions(['loadProducts']),
     getDiscount(price, preDiscPrice) {
-      return Math.floor(100 * ((preDiscPrice - price) / preDiscPrice));
+      return Math.floor(100 * (preDiscPrice - price) / preDiscPrice);
     },
     isDisc(product) {
       return product.price !== product.pre_disc_price;
@@ -1764,7 +1774,9 @@ export default {
 			margin:
 				top: auto
 
-	.pagination
+  .liked
+    fill: #E70068
+  .pagination
 		display: inline-block
 		padding:
 			top: 5px
