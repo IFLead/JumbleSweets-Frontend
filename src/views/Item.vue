9dfbd9a7529336276ecbc068f89db7a1b6fe5ee5
Gumbeat <leo2598@yandex.ua>
2018-09-16 17:08:47 +0300

    more fixes

--- src/views/Item.vue
+++ src/views/Item.vue
@@ -574,10 +574,10 @@ export default {
     },
   },
   metaInfo: {
-    // if no subcomponents specify a metaInfo.title, this title will be used
-    title: 'Default Title',
-    // all titles will be injected into this template
-    titleTemplate: '%s | My Awesome Webapp',
+    // // if no subcomponents specify a metaInfo.title, this title will be used
+    // title: 'Default Title',
+    // // all titles will be injected into this template
+    // titleTemplate: '%s | My Awesome Webapp',
   },
 };
 </script>
