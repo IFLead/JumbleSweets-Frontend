a20ccad56ef42ff5270f40fcd92f7ec8760be79d
Gumbeat <leo2598@yandex.ua>
2018-09-16 16:28:14 +0300

    fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import YmapPlugin from 'vue-yandex-maps';
+import Meta from 'vue-meta';
 import 'normalize.css/normalize.css';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
@@ -13,7 +14,12 @@ import './styles/element-variables.scss';
 Vue.config.productionTip = false;
 
 Vue.use(YmapPlugin);
-
+Vue.use(Meta, {
+  keyName: 'metaInfo', // the component option name that vue-meta looks for meta info on.
+  attribute: 'data-vue-meta', // the attribute name vue-meta adds to the tags it observes
+  ssrAttribute: 'data-vue-meta-server-rendered', // the attribute name that lets vue-meta know that meta info has already been server-rendered
+  tagIDKeyName: 'vmid', // the property name that vue-meta uses to determine whether to overwrite or append a tag
+});
 new Vue({
   router,
   store,
