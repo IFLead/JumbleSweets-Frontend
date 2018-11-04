bdbbd6217f2a10d22f2354d31f5e8bbb507e9f68
virvira <varviko.mx@gmail.com>
2018-11-05 00:36:37 +0300

    modals

---
+++ src/components/modals/ClearCartModal.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+      name: "ClearCartModal",
+      components: {},
+    }
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/components/modals/WishesModal.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+      name: "WishesModal",
+      components: {},
+    }
+</script>
+
+<style lang="sass">
+
+</style>
