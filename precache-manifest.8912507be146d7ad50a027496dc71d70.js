985abaf658e060c222afa267dad3ec6e1a31e261
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-12 11:16:43 +0000

    Deploy Art9Studio/JumbleSweets-Frontend to github.com/Art9Studio/JumbleSweets-Frontend.git:gh-pages

---
+++ precache-manifest.8912507be146d7ad50a027496dc71d70.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e4bd2c41ffcfac1f5dead6a3a4b287a",
+    "url": "/img/gleb-kozenko-170832-unsplash.6e4bd2c4.jpg"
+  },
+  {
+    "revision": "7496e03e0bda303eccd63ee0c2470e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90140edcb94f574c6f9f",
+    "url": "/js/app-legacy.3f340e80.js"
+  },
+  {
+    "revision": "52c0c8148cf7e7033a1e",
+    "url": "/js/chunk-vendors-legacy.907f5585.js"
+  },
+  {
+    "revision": "d9f82dec02c4e0688e2d1ab8490a6163",
+    "url": "/img/key.d9f82dec.svg"
+  },
+  {
+    "revision": "c6019cb92eb6ac21132e84b4a3fae99d",
+    "url": "/img/refresh_icon.c6019cb9.svg"
+  },
+  {
+    "revision": "8016d22e53acf1ef42f20147a707f58c",
+    "url": "/img/refresh_profile.8016d22e.svg"
+  },
+  {
+    "revision": "b022ff37c425108167edb87df081a9b2",
+    "url": "/img/white.b022ff37.jpg"
+  },
+  {
+    "revision": "a24fcf07c87b2e230b09f36fa0f1fb8d",
+    "url": "/img/user.a24fcf07.svg"
+  },
+  {
+    "revision": "bf9502044deb1ee243ba041dbc392082",
+    "url": "/img/rubbish-bin-delete-button.bf950204.svg"
+  },
+  {
+    "revision": "a1db2d1debaee1ea40d907bb6c55415e",
+    "url": "/img/registr_back.a1db2d1d.jpg"
+  },
+  {
+    "revision": "8b8700d7c125aafdd07f91c52bb4b8ea",
+    "url": "/img/shopping-cart.8b8700d7.svg"
+  },
+  {
+    "revision": "3613cc6e49bd9f24f06cd6ea197c51c1",
+    "url": "/img/right-arrow_profile_top.3613cc6e.svg"
+  },
+  {
+    "revision": "a9e4964e87ff51cf7c03aabc826ea38d",
+    "url": "/img/wedding-gift-profile.a9e4964e.svg"
+  },
+  {
+    "revision": "123ee46f3d8e069fa87b68cdd8e571ca",
+    "url": "/img/ukraine.123ee46f.jpg"
+  },
+  {
+    "revision": "642a332950bf7dbe036d5bfca8b1bdf4",
+    "url": "/img/shopping-purse-icon.642a3329.svg"
+  },
+  {
+    "revision": "e8c9b043394287f8273ae3c163eddd78",
+    "url": "/img/right-arrow_profile.e8c9b043.svg"
+  },
+  {
+    "revision": "e2751dd4f879bccb1f7645d8a7b2a0e7",
+    "url": "/img/user_back.e2751dd4.jpg"
+  },
+  {
+    "revision": "c7a8b104e897113503cb701940a20f73",
+    "url": "/img/tool.c7a8b104.svg"
+  },
+  {
+    "revision": "0253c4668c3946015defc1ddf24d12ee",
+    "url": "/img/sweet.0253c466.svg"
+  },
+  {
+    "revision": "ecabff06d416ab0fe06a491e71164c12",
+    "url": "/img/save-button-profile.ecabff06.svg"
+  },
+  {
+    "revision": "64d84474b771f2bf2d849e0ac15e3ac0",
+    "url": "/img/samuel-holt-490207-unsplash.64d84474.jpg"
+  },
+  {
+    "revision": "0b0a245fb30d87ca19dcf9b4a486f829",
+    "url": "/fonts/ProximaNova-Regular.0b0a245f.woff"
+  },
+  {
+    "revision": "791ce6d006f937822b2d77ee8478b9d5",
+    "url": "/img/filter-tool-black-shape.791ce6d0.svg"
+  },
+  {
+    "revision": "0ad51b546eaccd745962d968ce190493",
+    "url": "/img/http___pluspng.com_img-png_snickers-png-file-snickers-wrapped-png-589.0ad51b54.png"
+  },
+  {
+    "revision": "7ea502d83d898bf53a2d859f32488302",
+    "url": "/img/laura-marques-647991-unsplash.7ea502d8.jpg"
+  },
+  {
+    "revision": "34d1cf698a8718475cf9ac07195c05a6",
+    "url": "/img/left-arrow.34d1cf69.svg"
+  },
+  {
+    "revision": "52be03e0a71e03069464bd26ec2dec60",
+    "url": "/img/milka.52be03e0.png"
+  },
+  {
+    "revision": "ea7745f1e77fdb39be10d640f67566c1",
+    "url": "/img/marmalade-1248343_1920.ea7745f1.jpg"
+  },
+  {
+    "revision": "11546298f3eb87c4bae3914835a13c1f",
+    "url": "/img/joshua-ness-225844-unsplash.11546298.jpg"
+  },
+  {
+    "revision": "66ccf42e29fc4a1f40778d3c80eda6e5",
+    "url": "/img/google.66ccf42e.svg"
+  },
+  {
+    "revision": "6b6917d2d548f71dbd3ca9bc61de9a0c",
+    "url": "/img/phone-receiver.6b6917d2.svg"
+  },
+  {
+    "revision": "ca466bb5d10416ec1e2573297de5256f",
+    "url": "/img/logo-pink.ca466bb5.svg"
+  },
+  {
+    "revision": "3aba75e08c611abc897b64382b675c48",
+    "url": "/img/instagram.3aba75e0.svg"
+  },
+  {
+    "revision": "c7bcff52fd7ce9f6928f6c1bc0c0f2b5",
+    "url": "/img/menu.c7bcff52.svg"
+  },
+  {
+    "revision": "d149705c0ab743f21dc394e54adec24a",
+    "url": "/img/pickup.d149705c.jpg"
+  },
+  {
+    "revision": "6f16b9c6c3b7f5a3333a8a0337290935",
+    "url": "/img/kharkiv.6f16b9c6.jpg"
+  },
+  {
+    "revision": "c70eba0d287faa57ee0ed201f34aed47",
+    "url": "/img/frankie-cordoba-526925-unsplash.c70eba0d.jpg"
+  },
+  {
+    "revision": "59e04c38f2e6e7784700835499d4eed3",
+    "url": "/img/gift-box-profile.59e04c38.svg"
+  },
+  {
+    "revision": "a1b0d4954f230f16b17a77646f1e433b",
+    "url": "/img/joanna-kosinska-255355-unsplash.a1b0d495.jpg"
+  },
+  {
+    "revision": "f42817b1a72af62ade9e62557417058a",
+    "url": "/img/gift-box.f42817b1.svg"
+  },
+  {
+    "revision": "52dd1297e000b58276ecae075ccb902a",
+    "url": "/img/glass_back.52dd1297.jpg"
+  },
+  {
+    "revision": "123ee46f3d8e069fa87b68cdd8e571ca",
+    "url": "/img/john-mark-smith-134302-unsplash.123ee46f.jpg"
+  },
+  {
+    "revision": "d721f196cd8b69bbc74587a717d24f96",
+    "url": "/img/letter.d721f196.svg"
+  },
+  {
+    "revision": "354a3a820bb48b966bc8641230d53628",
+    "url": "/img/map.354a3a82.svg"
+  },
+  {
+    "revision": "f2ded66ea0e06982674cdd190fe75f13",
+    "url": "/img/envelope.f2ded66e.svg"
+  },
+  {
+    "revision": "92111bad3d61fd9f2daf95ec84abc325",
+    "url": "/img/fast.92111bad.svg"
+  },
+  {
+    "revision": "d149705c0ab743f21dc394e54adec24a",
+    "url": "/img/felix-russell-saw-234903-unsplash.d149705c.jpg"
+  },
+  {
+    "revision": "3d61406d9caa33b1d32851666e8fe622",
+    "url": "/img/favorite-heart-button.3d61406d.svg"
+  },
+  {
+    "revision": "51c3388ffc80327cec3debe7cc87aaba",
+    "url": "/img/3.51c3388f.png"
+  },
+  {
+    "revision": "77c0c68c086ba7b243ca150880ac50d6",
+    "url": "/img/about_back.77c0c68c.jpg"
+  },
+  {
+    "revision": "fd05e85087eb8e92c70cb1ed139667b4",
+    "url": "/img/candies-chocolates-delicious-867464.fd05e850.jpg"
+  },
+  {
+    "revision": "5c9224c8729ba100d1ede9329a00ffef",
+    "url": "/img/candies-confection-delicious-884509.5c9224c8.jpg"
+  },
+  {
+    "revision": "d6d9f5c27888d1ba336fbccdf4f224b7",
+    "url": "/img/drew-taylor-325456-unsplash.d6d9f5c2.jpg"
+  },
+  {
+    "revision": "42827e0fc937d778b8297c1888b0c087",
+    "url": "/img/christian-dubovan-686398-unsplash.42827e0f.jpg"
+  },
+  {
+    "revision": "ae2e71d2bb6461393fe530a1e926ff8c",
+    "url": "/img/annie-spratt-96529-unsplash.ae2e71d2.jpg"
+  },
+  {
+    "revision": "1307638254a35102aad3b02a68b0a1cd",
+    "url": "/img/anton-sharov-423618-unsplash.13076382.jpg"
+  },
+  {
+    "revision": "ed490806e204b0229d5386393d8240de",
+    "url": "/img/candy-bar-1735659_1920.ed490806.jpg"
+  },
+  {
+    "revision": "d11e16674d8674bdcf1246a4f51cbb3c",
+    "url": "/img/facebook-logo.d11e1667.svg"
+  },
+  {
+    "revision": "82a4b91ab73ee2a21d0b64160a420cf7",
+    "url": "/img/churros-2188871_1920.82a4b91a.jpg"
+  },
+  {
+    "revision": "c39b8e35d06cc369f398b4417e170e65",
+    "url": "/img/card-payment_profile.c39b8e35.svg"
+  },
+  {
+    "revision": "dbb46a20b0648461116269407689c819",
+    "url": "/img/delivery-box.dbb46a20.svg"
+  },
+  {
+    "revision": "c83b54c76295d929a42ff31fe2b341d9",
+    "url": "/img/eye-close-up.c83b54c7.svg"
+  },
+  {
+    "revision": "60cef9d80aaa1728ee6328f4e559266c",
+    "url": "/img/cancel.60cef9d8.svg"
+  },
+  {
+    "revision": "5e0ff2ff3aee42f19a897b576f74dbc2",
+    "url": "/img/12.5e0ff2ff.png"
+  },
+  {
+    "revision": "94844d28cbe812d7c070afd987f2e433",
+    "url": "/img/Logo-light.94844d28.svg"
+  },
+  {
+    "revision": "393f1562f3659e62244af6c95f0b0605",
+    "url": "/img/ProximaNova-Bold.393f1562.svg"
+  },
+  {
+    "revision": "88b9691213180b52230e2372ed3e5d6f",
+    "url": "/img/ProximaNova-Regular.88b96912.svg"
+  },
+  {
+    "revision": "8df2c6fba4192d2a122d975f517443f0",
+    "url": "/img/Sprite.8df2c6fb.png"
+  },
+  {
+    "revision": "e94e096934b1145b0083c871ea4f424c",
+    "url": "/fonts/ProximaNova-Regular.e94e0969.eot"
+  },
+  {
+    "revision": "7b8920eb5cc794b1269f798c0b798c9e",
+    "url": "/img/Calendar.7b8920eb.svg"
+  },
+  {
+    "revision": "4ef5613c279e8be75845f33511d0403f",
+    "url": "/img/Clock.4ef5613c.svg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "d670e5f6f99819330787beb38e9ce9fa",
+    "url": "/fonts/ProximaNova-Regular.d670e5f6.ttf"
+  },
+  {
+    "revision": "82bd63598e8ef2415e42854dd2d7ca31",
+    "url": "/fonts/ProximaNova-Regular.82bd6359.woff2"
+  },
+  {
+    "revision": "c54a320c27283a20ee4a3b857ca1473a",
+    "url": "/fonts/ProximaNova-Bold.c54a320c.woff2"
+  },
+  {
+    "revision": "876e70aebcf25b70b6b33b28e9afcece",
+    "url": "/fonts/ProximaNova-Bold.876e70ae.ttf"
+  },
+  {
+    "revision": "6543237802895abbb6c0af9229455eb4",
+    "url": "/fonts/ProximaNova-Bold.65432378.eot"
+  },
+  {
+    "revision": "3209fbf0af92523073d2b3f5be1998e9",
+    "url": "/fonts/ProximaNova-Bold.3209fbf0.woff"
+  },
+  {
+    "revision": "52c0c8148cf7e7033a1e",
+    "url": "/css/chunk-vendors.b8ba9fb0.css"
+  },
+  {
+    "revision": "90140edcb94f574c6f9f",
+    "url": "/css/app.7828aee2.css"
+  }
+];
