caf9f7e40e7556cfddf5e793103440ab6c51163a
Igor <IGOS.321@gmail.com>
2018-11-12 13:47:13 +0300

    opt

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const glob = require('glob-all');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 module.exports = {
   lintOnSave: true,
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config.optimization.sideEffects = true;
-    } else {
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     config.optimization.sideEffects = true;
+  //   } else {
 
-    }
+  //   }
+  // },
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js'),
+        ]),
+      }),
+    ],
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // GraphQL Loader
     // config.module
     //   .rule('graphql')
@@ -17,13 +30,11 @@ module.exports = {
     //   .use('graphql-tag/loader')
     //   .loader('graphql-tag/loader')
     //   .end()
-
     // const svgRule = config.module.rule('js');
     // svgRule.include = [ // use `include` vs `exclude` to white-list vs black-list
     //   path.resolve(__dirname, "src"), // white-list your app source files
     //   require.resolve("bootstrap-vue"), // white-list bootstrap-vue
     // ];
-
   },
   // chainWebpack: (config) => {
   //   config.module.rule('eslint').use('eslint-loader').options({
